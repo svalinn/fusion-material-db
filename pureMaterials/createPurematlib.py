9975b640a03bb00890e3246bf36a125ee6e86c7d
Paul Wilson <paul.wilson@wisc.edu>
2024-07-08 12:55:36 -0500

    stop writing HDF5 version
    Co-authored-by: Edgar-21 <84034227+Edgar-21@users.noreply.github.com>

--- pureMaterials/createPurematlib.py
+++ pureMaterials/createPurematlib.py
@@ -474,7 +474,6 @@ def main():
         pass  
         
     # write fnsf1d material library
-    mat_lib.write_hdf5("PureFusionMaterials_libv1.h5") # don't set datapath,nucpath...will be pyne default values
     mat_lib.write_json("PureFusionMaterials_libv1.json")
     # change datapath to be able to read with older version of uwuw_preproc
     #mat_lib.write_hdf5("PureFusionMaterials_libv1_old.h5",datapath='/materials', nucpath='/nucid')
